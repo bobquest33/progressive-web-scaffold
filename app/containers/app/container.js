da731b00e923bcce2f72529e51582e32f6817a9e
Mike Klemarewski <mikek@mobify.com>
2016-09-21 16:13:14 -0700

    Add example of i18n using react-intl

--- app/containers/app/container.js
+++ app/containers/app/container.js
@@ -5,7 +5,20 @@ import {IconSprite} from 'progressive-web-sdk/dist/components/icon'
 
 // import * as appActions from './actions'
 
+import {addLocaleData, FormattedDate, FormattedMessage, IntlProvider} from 'react-intl'
+import en from 'react-intl/locale-data/en'
+import ar from 'react-intl/locale-data/ar'
+
+const dict = {
+    'app.goodbye': 'Goodbye {name} :\'('
+}
+
 class App extends React.Component {
+
+    componentWillMount() {
+        addLocaleData([...en, ...ar])
+    }
+
     componentDidMount() {
         hidePreloader()
         // Dispatch an action to retrieve global content here
@@ -15,23 +28,37 @@ class App extends React.Component {
         let currentTemplate = `t-${this.props.children.props.route.routeName}`
 
         return (
-            <div id="outer-container" className="t-app">
-                <IconSprite />
+            <IntlProvider locale="ar" messages={dict}>
+                <div id="outer-container" className="t-app">
+                    <IconSprite />
 
-                <main id="page-wrap" className={currentTemplate}>
-                    <header>
-                        Header content
-                    </header>
+                    <main id="page-wrap" className={currentTemplate}>
+                        <header>
+                            Date:
+                            <FormattedDate
+                                value={new Date()}
+                                year='numeric'
+                                month='long'
+                                day='numeric'
+                                weekday='long'
+                            />
+                        </header>
 
-                    <div id="content">
-                        {this.props.children}
-                    </div>
+                        <FormattedMessage
+                            id="app.goodbye"
+                            values={{name: 'mike'}}
+                            />
 
-                    <footer>
-                        Footer content
-                    </footer>
-                </main>
-            </div>
+                        <div id="content">
+                            {this.props.children}
+                        </div>
+
+                        <footer>
+                            Footer content
+                        </footer>
+                    </main>
+                </div>
+            </IntlProvider>
         )
     }
 }
