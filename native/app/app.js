5d28b98da7c2ab281a1200cf858447532268ecfb
Kerr Marin Miller <kmarinmiller@mobify.com>
2016-12-30 11:52:44 -0800

    Add onboarding modal controller with dummy html

--- native/app/app.js
+++ native/app/app.js
@@ -5,14 +5,22 @@ import Application from 'progressive-app-sdk/application'
 import MobifyPreviewPlugin from 'progressive-app-sdk/plugins/mobifyPreviewPlugin'
 import PreviewController from 'progressive-app-sdk/controllers/previewController'
 
+// Local
 import baseConfig from './config/baseConfig'
 import TabBarController from './controllers/tabBarController'
 import {getInitialTabId} from './config/tabBarConfig'
+import OnboardingModalController from './controllers/onboarding/onboardingModalController'
 
 window.run = async function() {
     const runApp = async function() {
-        const tabBarController = await TabBarController.init()
+        const onboardingModalController = await OnboardingModalController.init()
 
+        // The onboarding modal can be configured to show only once
+        // (on first launch) by setting `{forced: false}` as the
+        // parameter for onboardingModalController.show()
+        onboardingModalController.show({forced: true})
+
+        const tabBarController = await TabBarController.init()
         await Application.setMainViewPlugin(tabBarController.viewPlugin)
 
         const initialTabId = getInitialTabId()
