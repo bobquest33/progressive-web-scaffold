bc0e776e5ba0e12182a6464eca4efa520408a492
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-01-20 15:34:54 -0800

    Add comment on index.js

--- native/onboarding/src/index.js
+++ native/onboarding/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import Onboarding from './components/Onboarding'
 import './index.scss'
 
+// This timeout is a side-effect of Android's issues with 
+// touchStart not working 
 setTimeout(function() { 
   ReactDOM.render(
     <Onboarding />,
